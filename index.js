3b3e17c95e1bb7098416f6c6d9898742f0593718
MOKSHsethi <mokshsethi7428@gmail.com>
2022-06-01 15:56:27 +0530

    change

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const app = require("./app");
 const path = require('path');
 const dotenv = require("dotenv").config();
-const port = 4000;
+const port = process.env.PORT || 4000;
 const database = require('./config/mongoose')
 
 database();
+app.get('/', (req, res) => {
+    res.send('Hi i am from crypto')
+})
 
 
 app.listen(port, () => {
